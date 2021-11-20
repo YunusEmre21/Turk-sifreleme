2af91fee08ecfa12bf00d5b44e6e67111354fa13
YunusEmre21 <93905593+YunusEmre21@users.noreply.github.com>
2021-11-20 19:17:12 +0300

    Update başlat.sh

--- başlat.sh
+++ başlat.sh
@@ -1,1 +1,34 @@
-echo deneme
+clear
+
+echo instagram/byunussluemre
+     github.com/YunusEmre21
+     
+echo [1] Termux Güncelle
+
+
+
+
+read -p "İşlem Numarası: " islem
+if [[ $islem == 1 || $islem == 01 ]]; then
+pkg install git -y
+pkg install python python2 -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install pip pip2 -y
+pkg install clang -y
+pkg install vim -y
+pkg install nano -y
+apt install proot -y
+pkg install cat -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install perl -y
+apt update
+apt upgrade -y
+clear
+
+echo kurulum bitti
+sleep 2
+     
