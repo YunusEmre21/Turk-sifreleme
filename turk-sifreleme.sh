bc306202a00a8d90b3b69b333ebe42ecfac93081
YunusEmre21 <93905593+YunusEmre21@users.noreply.github.com>
2021-11-20 19:03:41 +0300

    Create turk-sifreleme.sh

---
+++ turk-sifreleme.sh
@@ -0,0 +1,37 @@
+
+
+
+
+
+echo -e '\033[31;40;1m 
+		   ─────█─▄▀█──█▀▄─█─────
+		   ────▐▌──────────▐▌────
+		   ────█▌▀▄──▄▄──▄▀▐█────
+		   ───▐██──▀▀──▀▀──██▌───
+		   ──▄████▄──▐▌──▄████▄──
+      \e[35m      ☠         Code  : YamanEfkar     ☠
+	  \e[34m☠	     Github : YamanEfkar       ☠
+	 \e[33m☠  	    Youtube : YamanEfkar        ☠
+	\e[32m☠ 	   İnstagram : @yamanefkarr      ☠
+	
+	\e[36m 	 ☾★ ☾★ Türk Siber Timi ☾★ ☾★	
+\e[32m[1]\e[33mTermux Güncelle		\e[32m[12]\e[33mFindUser 
+\e[32m[2]\e[33mXerxes			\e[32m[13]\e[33mNmap
+\e[32m[3]\e[33mHunner			\e[32m[14]\e[33mA-Rat
+\e[32m[4]\e[33mNgrok			\e[32m[15]\e[33mCamer-Tr	
+\e[32m[5]\e[33mDork-Tarayıcı		\e[32m[16]\e[33mWeeman
+\e[32m[6]\e[33mSqlMap			\e[32m[17]\e[33mOSIF
+\e[32m[7]\e[33mAdmin-Panel-Finder		\e[32m[18]\e[33mTelefon-Pentest
+\e[32m[8]\e[33mMetasploit			\e[32m[19]\e[33mTurk-Sploit	
+\e[32m[9]\e[33mİPGeoLocation		\e[32m[20]\e[33mMd-5 Force
+\e[32m[10]\e[33mTc-Kimlik			\e[32m[21]\e[33mİnsta-brute	
+\e[32m[11]\e[33mİnstagram-Spam		\e[32m[22]\e[33mTermux-Debian
+
+
+
+
+
+
+    
+    
+    
