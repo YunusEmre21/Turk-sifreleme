ea6c1129d24ca58b135a2dafb226b3714b28a150
YunusEmre21 <93905593+YunusEmre21@users.noreply.github.com>
2021-11-21 16:12:30 +0300

    Delete başlat.sh

--- başlat.sh
+++
@@ -1,7 +0,0 @@
-clear 
-echo Bu dosyalar etik amaçlar doğrultusunda yapılmıştır.Bu dosyalar kullanılarak yapılan hiçbir suçtan ben sorumlu değilim.Bu dosyaları kullanarak bunları okumuş ve kabul etmiş sayılırsınız.
-
-read -p "İşlem Numarası: " islem
-
-if [[ $islem == 1 || $islem == 01 ]]; then
-cd $Turk-sifreleme
